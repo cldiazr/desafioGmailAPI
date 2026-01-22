785834eac2dd90c1eb85fda7d0b2a87acf9a8007
Christian Diaz <rivalch2015@gmail.com>
2026-01-22 19:18:29 -0400

    Se agregan las dependencias en un archivo. ademas se agregan nuevas funciones de filtro para la bandeja de entrada de gmail y se agrega tambien la bandeja SPAM

--- viewMessagesTest.py
+++ viewMessagesTest.py
@@ -4,6 +4,10 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+import base64
+from email import policy
+from email.parser import BytesParser
+import re
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
@@ -34,22 +38,52 @@ def main():
         # Call the Gmail API
         service = build("gmail", "v1", credentials=creds)
         results = (
-            service.users().messages().list(userId="me", labelIds=["INBOX"]).execute()
+            service.users().messages().list(userId="me", includeSpamTrash=True).execute()
         )
         messages = results.get("messages", [])
 
         if not messages:
             print("No messages found.")
             return
-
         print("Messages:")
         for message in messages:
-            print(f'Message ID: {message["id"]}')
             msg = (
-                service.users().messages().get(userId="me", id=message["id"]).execute()
+                service.users().messages().get(userId="me", id=message["id"], format='raw').execute()
             )
-            print(f'  Subject: {msg["snippet"]}')
-
+            msg_bytes = base64.urlsafe_b64decode(msg['raw'].encode('ASCII'))
+            mime_msg = BytesParser(policy=policy.default).parsebytes(msg_bytes)
+            if mime_msg.is_multipart():
+                for part in mime_msg.walk():
+                    content_type = part.get_content_type()
+                    content_disposition = str(part.get("Content-Disposition"))
+                    if content_type == "text/plain" and "attachment" not in content_disposition:
+                        body = part.get_payload(decode=True).decode()
+                        if not((re.search(r'\bgoogle.com\b', mime_msg['from'])) and (re.search(r'\bcashea.app\b', mime_msg['from'])) and (re.search(r'\bspicosmos.com\b', mime_msg['from']))):
+                            if re.search(r'\bcontraseña\b', body.lower()):
+                                print(f"Asunto: {mime_msg['subject']}")
+                                print(f"De: {mime_msg['from']}")
+                                print("Palabra: Contraseña")
+                            if re.search(r'\bconfidencial\b', body.lower()):
+                                print(f"Asunto: {mime_msg['subject']}")
+                                print(f"De: {mime_msg['from']}")
+                                print("Palabra: Confidencial")
+                    elif "attachment" in content_disposition:
+                        filename = part.get_filename()
+                        if filename:
+                            if filename.lower().replace(" ", "").endswith(".pdf"):
+                                print(f"Asunto: {mime_msg['subject']}")
+                                print(f"Archivo adjunto encontrado: {filename}")
+            else:
+                body = mime_msg.get_payload(decode=True).decode()
+                if not((re.search(r'\bgoogle.com\b', mime_msg['from'])) and (re.search(r'\bcashea.app\b', mime_msg['from'])) and (re.search(r'\bspicosmos.com\b', mime_msg['from']))):
+                            if re.search(r'\bcontraseña\b', body.lower()):
+                                print(f"Asunto: {mime_msg['subject']}")
+                                print(f"De: {mime_msg['from']}")
+                                print("Palabra: Contraseña")
+                            if re.search(r'\bconfidencial\b', body.lower()):
+                                print(f"Asunto: {mime_msg['subject']}")
+                                print(f"De: {mime_msg['from']}")
+                                print("Palabra: Confidencial")
     except HttpError as error:
         # TODO(developer) - Handle errors from gmail API.
         print(f"An error occurred: {error}")
